8d62b932f5670ecfb91016bd95f0c215b7f1f39b
sahaskamble <sahaskamble282@gmail.com>
2024-03-20 18:58:13 +0530

    second UI commit

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,10 +1,20 @@
 import Navbar from "./Navbar";
+import "../assets/css/index.css"
 
 export default function Header() {
 	return(
 		<header>
 			<div>
 				<Navbar />
+				<div id="scroller" className="w-full overflow-scroll h-20 flex justify-between items-center whitespace-nowrap p-2">
+					<div className="w-[33%] h-auto inline-flex flex-col items-center cursor-pointer"><span className="text-2xl">🎂</span><span className="underline">Birthday</span></div>
+					<div className="w-[33%] h-auto inline-flex flex-col items-center cursor-pointer"><span className="text-2xl">🎈</span><span className="underline">Aniversery</span></div>
+					<div className="w-[33%] h-auto inline-flex flex-col items-center cursor-pointer"><span className="text-2xl">👶</span><span className="underline">Baby shower</span></div>
+					<div className="w-[33%] h-auto inline-flex flex-col items-center cursor-pointer"><span className="text-2xl">👶</span><span className="underline">Haldi</span></div>
+					<div className="w-[33%] h-auto inline-flex flex-col items-center cursor-pointer"><span className="text-2xl">👶</span><span className="underline">Mr Beast Fest</span></div>
+					<div className="w-[33%] h-auto inline-flex flex-col items-center cursor-pointer"><span className="text-2xl">👶</span><span className="underline">Blind Date</span></div>
+					<div className="w-[33%] h-auto inline-flex flex-col items-center cursor-pointer"><span className="text-2xl">👶</span><span className="underline">Mr Beast Challenge</span></div>
+				</div>
 			</div>
 		</header>
 	)
